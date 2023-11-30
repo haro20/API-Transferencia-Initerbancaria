3841254758f0a94161a061cc8e937111b403c0b0
haro20 <armando_haro1@hotmail.com>
2023-11-30 16:29:14 -0500

    Han paso los test de: - TransferServiceImplement - TransferControllerTest
    GETS Y POSTS (OK)

--- src/main/java/com/cruz/bbva/mapper/TransferMapper.java
+++ src/main/java/com/cruz/bbva/mapper/TransferMapper.java
@@ -3,10 +3,14 @@ package com.cruz.bbva.mapper;
 import com.cruz.bbva.DTO.TransferDTO;
 import com.cruz.bbva.entity.Transfer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 
 @Mapper(componentModel = "spring")
 public interface TransferMapper {
 
+    @Mapping(target = "idTransfer", source = "transfer.idTransfer")
     TransferDTO transferToTransferDTO(Transfer transfer);
+
     Transfer TransferDTOTotransfer(TransferDTO transferDTO);
 }
